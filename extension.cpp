3ee52e1bddfcaf263b4d02e81f1a952c9e2edf35
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-03-25 16:52:37 +0900

    Update extension.cpp

--- extension.cpp
+++ extension.cpp
@@ -1,5 +1,4 @@
 #include "pxt.h"
-#include "MicroBitI2C.h"
 
 #define MPU 0x68
 
@@ -8,17 +7,18 @@ namespace mpuext {
     //% shim=mpuext::read_frame
     Buffer read_frame() {
 
-        MicroBitI2C* i2c = uBit.i2c;
+        // I2C ポインタ
+        auto i2c = &uBit.i2c;
 
-        // Write register 0x3B (ACCEL_XOUT_H)
+        // 1. ACCEL_XOUT_H (0x3B) を指定
         char reg = 0x3B;
-        int r = i2c->write(MPU << 1, &reg, 1, true); // repeated start
+        i2c->write(MPU << 1, &reg, 1, true);
 
-        // Read 14 bytes
+        // 2. 14バイト読み取り
         char data[14];
-        r = i2c->read(MPU << 1, data, 14);
+        i2c->read(MPU << 1, data, 14);
 
-        // Return as ManagedBuffer
+        // 3. Buffer にして返す
         return ManagedBuffer((uint8_t*)data, 14);
     }
 }
